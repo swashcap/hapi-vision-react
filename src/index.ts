938b833dab3284cb26ad2c2b82098a31dbddff17
Cory Reed <swashcap@gmail.com>
2019-04-30 10:39:48 -0700

    feat: add vision plugin

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,14 @@
 import 'hard-rejection/register'
 
 import hapi from '@hapi/hapi'
+import vision from '@hapi/vision'
 
 const init = async () => {
   const server = new hapi.Server({
-    host: 'localhost',
     port: 3000
   })
 
+  await server.register(vision)
   await server.start()
 
   console.log(`Server running on ${server.info.uri}`)
